b409e2871545886559a289d9f4458edee960f5e6
Guimaaz <gustavoguimaraesr@gmail.com>
2025-02-28 20:30:16 -0300

    add : horarios de funcionamento em prompt_horarios

--- prompts.py
+++ prompts.py
@@ -14,13 +14,6 @@ prompt_restaurante = """
 
 prompt_dos_Horarios = """
         
-        Você é um atendente virtual do restaurante do Poliedro. Sua função é fornecer respostas claras, educadas e objetivas sobre o cardápio, pedidos, entregas e horários de funcionamento. 
-
-        Se alguém perguntar sobre assuntos que não estejam relacionados ao restaurante, responda educadamente com: 
-        "Me desculpe, só consigo responder perguntas relacionadas ao restaurante, como cardápio, pedidos, entregas e horários de funcionamento."
-
-        Sempre mantenha um tom amigável e profissional, ajudando o cliente de forma eficiente e direta. 
-
         os horario de funcionamento são das 18-22, e aos finais de semana, 19-20
         
         """
