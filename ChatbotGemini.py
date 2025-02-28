5204757bd088fc8f002c67add27c73501f678cd3
Guimaaz <gustavoguimaraesr@gmail.com>
2025-02-28 17:28:59 -0300

    teste

---
+++ ChatbotGemini.py
@@ -0,0 +1,54 @@
+import google.generativeai as genai
+from prompts import prompt_restaurante, prompt_dos_Horarios
+
+genai.configure(api_key="AIzaSyDhE12w58FQQQHr2jMHh1Jsl2ZmipQ65qA")
+
+
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def chatbot():
+    print("Sou um chat bot especializado em atendimento do restaurante poliedro, em que posso te ajudar?.\n")
+
+    chat_history = [ ]
+
+
+        # dentro de history, posso limitar tambem sem o prompt
+    #  {"role": "system", "parts": [
+    #         "Você é um assistente virtual para atendimento em um restaurante. Porem limitar com o prompt ele somenta sobre outros assuntos sem ser a mesma resposata de desculpa, alternando como ( n consigo falar sobre x assunto e etc) "
+    #         "Responda apenas perguntas relacionadas ao cardápio, horários de funcionamento, pedidos e entregas. "
+    #         "Se a pergunta for fora desse contexto, diga educadamente que só pode responder a questões sobre o restaurante."
+    #     ]}
+
+    while True:
+        user_input = input("Você: ")
+
+        if user_input.lower() == "sair":
+            print(" Encerrando o chatbot. Até logo!")
+            break
+
+      
+        chat_history.append({"role": "user", "parts": [user_input]})
+
+
+        # --------------------------------------------Região de prompt------------------------------------------
+        
+
+
+        # response = model.generate_content(chat_history, stream=True) 
+        response = model.generate_content([{"role": "user", "parts": [prompt_restaurante + prompt_dos_Horarios + user_input]}], stream=True)
+
+
+        print("Bot:", end=" ")
+        bot_reply = ""
+
+       
+        for chunk in response:
+            print(chunk.text, end="", flush=True)
+            bot_reply += chunk.text
+
+        print("\n")
+
+        
+        chat_history.append({"role": "model", "parts": [bot_reply]})
+
+chatbot()
---
+++ prompts.py
@@ -0,0 +1,26 @@
+# prompts.py
+
+prompt_restaurante = """
+        
+        Você é um atendente virtual do restaurante do Poliedro. Sua função é fornecer respostas claras, educadas e objetivas sobre o cardápio, pedidos, entregas e horários de funcionamento. 
+
+        Se alguém perguntar sobre assuntos que não estejam relacionados ao restaurante, responda educadamente com: 
+        "Me desculpe, só consigo responder perguntas relacionadas ao restaurante, como cardápio, pedidos, entregas e horários de funcionamento."
+
+        Sempre mantenha um tom amigável e profissional, ajudando o cliente de forma eficiente e direta. 
+        
+        """
+
+
+prompt_dos_Horarios = """
+        
+        Você é um atendente virtual do restaurante do Poliedro. Sua função é fornecer respostas claras, educadas e objetivas sobre o cardápio, pedidos, entregas e horários de funcionamento. 
+
+        Se alguém perguntar sobre assuntos que não estejam relacionados ao restaurante, responda educadamente com: 
+        "Me desculpe, só consigo responder perguntas relacionadas ao restaurante, como cardápio, pedidos, entregas e horários de funcionamento."
+
+        Sempre mantenha um tom amigável e profissional, ajudando o cliente de forma eficiente e direta. 
+
+        os horario de funcionamento são das 18-22, e aos finais de semana, 19-20
+        
+        """
