3d39c37cb54762965c2300c6447215f46d0e1cfc
Guimaaz <gustavoguimaraesr@gmail.com>
2025-05-16 21:53:41 -0300

    observation  : if we put "localhost" in api_base_url, the app dont will work in mobile devices, we need use the real ip IPV4 to use in another devices ( the wifi need to be same as in notebook ) - so now i changed localhost to my onw ip

--- chatbot_frontend/utils/api.js
+++ chatbot_frontend/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'http://192.168.15.7:5000';
 
 export const sendMessage = async (messageData) => {
   try {
