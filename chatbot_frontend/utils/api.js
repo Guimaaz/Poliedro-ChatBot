c3f4d3c35a4690a42d5a72aede994381ad482e78
GuiMD <gmdurt14@gmail.com>
2025-05-21 09:36:11 -0300

    100% atualizado

--- chatbot_frontend/utils/api.js
+++ chatbot_frontend/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://172.20.10.3:5000';
+export const API_BASE_URL = 'http://10.2.129.29:5000';
 
 export const sendMessage = async (messageData) => {
   try {
